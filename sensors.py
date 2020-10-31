e3bb7068ff3afb42053e5f862c13101aaaee2723
Logan Riffell <lkriffell@gmail.com>
2020-10-31 13:06:08 -0600

    Update temperature request to use the new temperature sensor enum

--- sensors.py
+++ sensors.py
@@ -54,7 +54,7 @@ while True:
 
     if sensor_ids.has_key('Temperature & Humidity'):
 	    data['temp'],data['humidity'] = grovepi.dht(dht_sensor,0)
-        requests.post("https://solar-garden-be.herokuapp.com/api/v1/garden_healths", params={'sensor_id': sensor_ids['Temperature & Humidity'], 'reading_type': 'moisture', 'reading': data['temp']})
+        requests.post("https://solar-garden-be.herokuapp.com/api/v1/garden_healths", params={'sensor_id': sensor_ids['Temperature & Humidity'], 'reading_type': 'temperature', 'reading': data['temp']})
 
     grovepi.ledBar_setBits(ledbar, val % 1024)
     val += 1
